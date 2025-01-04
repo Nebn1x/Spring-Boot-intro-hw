7aa74b2981c6e67301528d205449532fef552bff
User <bob@i.ua>
2025-01-04 19:18:32 +0200

    done

--- src/main/java/springboot/exeptions/CustomGlobalExceptionHandler.java
+++ src/main/java/springboot/exeptions/CustomGlobalExceptionHandler.java
@@ -15,6 +15,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
             MethodArgumentNotValidException ex,
