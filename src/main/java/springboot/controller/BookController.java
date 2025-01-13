1f713ee9fcfd7c03ddd6ab2ac17bf4858a6ed42a
User <bob@i.ua>
2025-01-13 19:31:31 +0200

    fixed

--- src/main/java/springboot/controller/BookController.java
+++ src/main/java/springboot/controller/BookController.java
@@ -1,7 +1,6 @@
 package springboot.controller;
 
 import jakarta.validation.Valid;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
