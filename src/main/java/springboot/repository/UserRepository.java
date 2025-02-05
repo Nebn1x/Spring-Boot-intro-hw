41fb38d5af9f923755ea31e6b3627f93bc760008
User <bob@i.ua>
2025-02-05 14:20:06 +0200

    .

--- src/main/java/springboot/repository/UserRepository.java
+++ src/main/java/springboot/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package springboot.repository;
 
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import springboot.model.User;
